04402dcb5de84aa47fc962e86dd675f17b88066d
ToddThomson <achilles@telus.net>
2015-03-27 12:24:10 -0700

    npm files
    Initial npm sync

---
+++ index.js
@@ -0,0 +1,9 @@
+function TsProject(files, options) {
+    console.log("Hello World");
+}
+TsProject.prototype.require = function (file, opts) {
+};
+TsProject.prototype.compile = function (file, opts) {
+};
+module.exports = TsProject;
+//# sourceMappingURL=index.js.map
