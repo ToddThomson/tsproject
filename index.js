0bf001a0146eec1663af07599ea7ddfab30618eb
ToddThomson <achilles@telus.net>
2015-04-07 10:42:19 -0700

    Removed files

--- index.js
+++
@@ -1,9 +0,0 @@
-function TsProject(files, options) {
-    console.log("Hello World");
-}
-TsProject.prototype.require = function (file, opts) {
-};
-TsProject.prototype.compile = function (file, opts) {
-};
-module.exports = TsProject;
-//# sourceMappingURL=index.js.map
