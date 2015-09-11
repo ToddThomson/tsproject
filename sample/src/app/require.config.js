6242de58e68c83b1a9f01a030b8253dd4e68ed87
ToddThomson <achilles@telus.net>
2015-09-11 10:20:22 -0700

    Require.config.js now included in sample. Fixes issue #41
    Git ignore file referenced incorrect path to require.config.js

---
+++ sample/src/app/require.config.js
@@ -0,0 +1,13 @@
+﻿// require.js looks for the following global when initializing
+var require = {
+    baseUrl: ".",
+    paths: {
+        "bootstrap": "bower_modules/components-bootstrap/js/bootstrap.min",
+        "jquery": "bower_modules/jquery/dist/jquery",
+        "knockout": "bower_modules/knockout/dist/knockout",
+        "text": "bower_modules/requirejs-text/text"
+    },
+    shim: {
+        "bootstrap": { deps: ["jquery"] }
+    }
+};
