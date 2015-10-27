45d1d2dfdc0d4bcad298d6e098f988021589a298
Rich Tebb <rich.tebb@dyson.com>
2015-10-27 11:18:38 +0000

    Refactor build to output to the dist/ folder

---
+++ build/tasks/build.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var paths = require('../paths');
+var tsproject = require( 'tsproject' );
+var debug = require('gulp-debug');
+var ignore = require('gulp-ignore');
+var rename = require('gulp-rename');
+var tsd = require('gulp-tsd');
+var chalk = require('chalk');
+
+gulp.task('tsd', function (callback) {
+  callback();
+  return;
+  // tsd({
+  //     command: 'reinstall',
+  //     config: './tsd.json'
+  // }, callback);
+
+  // OR
+  // return gulp.src('./gulp_tsd.json').pipe(tsd());
+});
+
+gulp.task('build', ['tsd'], function () {
+  
+  console.log( '\nPlaced optimized files in ' + chalk.magenta( paths.output + '\n' ) );
+  // path to named configuration file provided..
+  return tsproject.src( paths.sourceTsConfig, {
+      logLevel: 2,
+      "compilerOptions": {
+        "module": "commonjs",
+        "target": "es5"
+      }
+    })
+    .pipe(rename({dirname: "",}))
+    .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('default', function(callback) {
+  return runSequence(
+    'clean',
+    ['build'],
+    callback
+  );
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,3 @@
-﻿// Node related
-var chalk = require('chalk');
-
-// Gulp related
-var gulp = require( 'gulp' ),
-    rimraf = require( 'gulp-rimraf' ),
-    uglify = require( 'gulp-uglify' ),
-    tsproject = require( './src/tsproject' );
-
-gulp.task( 'ts', function() {
-    return tsproject.src( './src/tsconfig.json', { logLevel: 4 } )
-        .pipe( gulp.dest( './dist' ) );
-} );
-
-// Removes all files from ./dist/
-gulp.task( 'clean', function() {
-    return gulp.src( './dist/**/*', { read: false } )
-      .pipe( rimraf() );
-} );
-
-gulp.task( 'default', [ 'ts'], function( callback ) {
-    console.log( '\nPlaced optimized files in ' + chalk.magenta( 'dist/\n' ) );
-    callback();
-} );
+﻿// all gulp tasks are located in the ./build/tasks directory
+// gulp configuration is in files in ./build directory
+require('require-dir')('build/tasks');
