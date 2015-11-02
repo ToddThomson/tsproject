c4d041e3d29dd3355f21c46631e44d216907dc05
Rich Tebb <rich.tebb@dyson.com>
2015-11-02 13:04:11 +0000

    Decouple tsd task from compile task

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -8,15 +8,16 @@ gulp.task('tsd', function (callback) {
   return gulp.src('./gulp_tsd.json').pipe( tsd() );
 });
 
-gulp.task('compile', ['tsd'], function () {
+gulp.task('compile', function () {
   return tsproject.src( paths.sourceTsConfig )
     .pipe( gulp.dest( paths.output ) );
 });
 
 gulp.task('build', function(callback) {
   return runSequence(
-    'clean',
-    ['compile'],
+    'clean', 
+    'tsd',
+    'compile',
     callback
   );
 });
