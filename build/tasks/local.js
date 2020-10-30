291b17891a1b3da5efb4585b6e252ca2ef1bb78b
Todd Thomson <achilles@telus.net>
2020-10-30 11:20:08 -0700

    Added build tasks for testing. Progress towards #105

---
+++ build/tasks/local.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const gulp = require( 'gulp' );
+const paths = require( '../paths' );
+const tsproject = require( '../../built/src/bundle/tsproject' );
+
+gulp.task( 'local-bundle', function ( cb )
+{
+    return tsproject.src( paths.sourceTsConfig )
+        .pipe( gulp.dest( paths.output ) );
+
+    cb();
+} );
---
+++ build/tasks/src.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const gulp = require( 'gulp' );
+const paths = require( '../paths' );
+const tsproject = require( '../../src/tsproject' );
+
+gulp.task( 'src-bundle', function ( cb )
+{
+    return tsproject.src( paths.sourceTsConfig )
+        .pipe( gulp.dest( paths.output ) );
+
+    cb();
+} );
